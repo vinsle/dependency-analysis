aa3f690b924192cb82320523e7b0f68277afb207
vinsle@gmail.com <vinsle@gmail.com@23fb0dd6-b554-2105-1b3a-678f624095da>
2010-08-17 10:02:20 +0000

    renaming

--- trunk/src/tests/depend_test/MockFileObserver.h
+++ trunk/src/tests/depend_test/MockFileObserver.h
@@ -6,8 +6,8 @@
 //  http://www.boost.org/LICENSE_1_0.txt)
 //
 
-#ifndef depend_test_MockLibrary_h
-#define depend_test_MockLibrary_h
+#ifndef depend_test_MockFileObserver_h
+#define depend_test_MockFileObserver_h
 
 #include "depend/FileObserver_ABC.h"
 
@@ -26,4 +26,4 @@ MOCK_BASE_CLASS( MockFileObserver, FileObserver_ABC )
 
 }
 
-#endif // depend_test_MockLibrary_h
+#endif // depend_test_MockFileObserver_h
