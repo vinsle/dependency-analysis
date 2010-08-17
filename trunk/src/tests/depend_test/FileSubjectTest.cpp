0d7d0c68b232a6e3e881f8090f8d9f1b8b65a7c4
vinsle@gmail.com <vinsle@gmail.com@23fb0dd6-b554-2105-1b3a-678f624095da>
2010-08-17 09:37:53 +0000

    add extensions filter on file visitor add recursive directory browsing on file visitor

--- trunk/src/tests/depend_test/FileSubjectTest.cpp
+++ trunk/src/tests/depend_test/FileSubjectTest.cpp
@@ -9,15 +9,18 @@
 #include "depend_test_pch.h"
 #include "depend/FileVisitor.h"
 #include "MockFileObserver.h"
+#include <boost/assign.hpp>
 
 using namespace depend;
 
-BOOST_AUTO_TEST_CASE( file_subject_lists_all_include_files_and_notify_listeners )
+BOOST_AUTO_TEST_CASE( file_visitor_lists_all_include_files_and_notify_listeners )
 {
-    FileVisitor visitor;
+    const std::vector< std::string > extensions = boost::assign::list_of( ".h" )( ".hpp" );
+    FileVisitor visitor( extensions );
     MockFileObserver observer;
     visitor.Register( observer );
     MOCK_EXPECT( observer, Notify ).once().with( "header.h" );
+    MOCK_EXPECT( observer, Notify ).once().with( "header.hpp" );
     MOCK_EXPECT( observer, Notify ).once().with( "module/module-header.h" );
     visitor.Visit( BOOST_RESOLVE( "file_subject_lists_all_include_files_and_notify_listeners/" ) );
     visitor.Unregister( observer );
