5b08ce55f58582e18d714a0fd41aea345b6821ed
vinsle@gmail.com <vinsle@gmail.com@23fb0dd6-b554-2105-1b3a-678f624095da>
2010-08-17 10:54:24 +0000

    depend application now lists all files on std::cout

--- trunk/src/applications/depend_app/main.cpp
+++ trunk/src/applications/depend_app/main.cpp
@@ -6,7 +6,11 @@
 //  http://www.boost.org/LICENSE_1_0.txt)
 //
 
-#include <xeumeuleu/xml.hpp>
+#include "depend/ModuleVisitor.h"
+#include "depend/ModuleObserver_ABC.h"
+#include "depend/FileVisitor.h"
+#include "depend/FileObserver_ABC.h"
+#include <boost/assign.hpp>
 #include <boost/program_options.hpp>
 #include <boost/filesystem.hpp>
 #include <iostream>
@@ -20,19 +24,66 @@ namespace
     {
         bpo::options_description desc( "Allowed options" );
         desc.add_options()
-            ( "help"         , "produce help message" )
-            ( "configuration", bpo::value< std::string >(), "set configuration file" );
+            ( "help"                             , "produce help message" )
+            ( "path", bpo::value< std::string >(), "set path directory" );
         bpo::positional_options_description p;
-        p.add( "configuration", 1 );
+        p.add( "path", 1 );
         bpo::variables_map vm;
         bpo::store( bpo::command_line_parser( argc, argv ).options( desc ).positional( p ).run(), vm );
         bpo::notify( vm );
         if( vm.count( "help" ) )
             std::cout << desc << std::endl;
-        else if( ! vm.count( "configuration" ) )
-            throw std::invalid_argument( "Invalid application option argument: missing configuration file" );
+        else if( ! vm.count( "path" ) )
+            throw std::invalid_argument( "Invalid application option argument: missing path file" );
         return vm;
     }
+    class FileObserver : private depend::FileObserver_ABC
+    {
+    public:
+        FileObserver( depend::FileVisitor& visitor, const std::string& module )
+            : visitor_( visitor )
+            , module_ ( module )
+        {
+            visitor_.Register( *this );
+        }
+        ~FileObserver()
+        {
+            visitor_.Unregister( *this );
+        }
+    private:
+        virtual void Notify( const std::string& path )
+        {
+            std::cout << module_ << ":" << path << std::endl;
+        }
+    private:
+        depend::FileVisitor& visitor_;
+        const std::string module_;
+    };
+    class ModuleObserver : private depend::ModuleObserver_ABC
+    {
+    public:
+        ModuleObserver( depend::ModuleVisitor& visitor, const std::string& path )
+            : visitor_( visitor )
+            , path_   ( path )
+        {
+            visitor_.Register( *this );
+        }
+        ~ModuleObserver()
+        {
+            visitor_.Unregister( *this );
+        }
+    private:
+        virtual void Notify( const std::string& module )
+        {
+            const std::vector< std::string > extensions = boost::assign::list_of( ".h" )( ".hpp" )( ".inl" )( ".cpp" )( ".hh" )( ".cc" );
+            depend::FileVisitor visitor( extensions );
+            FileObserver observer( visitor, module );
+            visitor.Visit( path_ + "/" + module );
+        }
+    private:
+        depend::ModuleVisitor& visitor_;
+        const std::string path_;
+    };
 }
 
 int main( int argc, char* argv[] )
@@ -42,9 +93,10 @@ int main( int argc, char* argv[] )
         const bpo::variables_map vm = ParseCommandLine( argc, argv );
         if( vm.count( "help" ) )
             return EXIT_SUCCESS;
-        const std::string configuration = vm[ "configuration" ].as< std::string >();
-        xml::xifstream xis( configuration, xml::internal_grammar() );
-        xis >> xml::start( "configuration" );
+        const std::string path = vm[ "path" ].as< std::string >();
+        depend::ModuleVisitor visitor;
+        ModuleObserver observer( visitor, path );
+        visitor.Visit( path );
         return EXIT_SUCCESS;
     }
     catch( std::exception& e )
