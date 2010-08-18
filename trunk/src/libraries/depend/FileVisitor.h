35b0d93b81fed18585c69437fc85a416104827f3
vinsle@gmail.com <vinsle@gmail.com@23fb0dd6-b554-2105-1b3a-678f624095da>
2010-08-18 10:06:46 +0000

    refactoring

--- trunk/src/libraries/depend/FileVisitor.h
+++ trunk/src/libraries/depend/FileVisitor.h
@@ -9,17 +9,19 @@
 #ifndef depend_FileVisitor_h
 #define depend_FileVisitor_h
 
-#include "FileVisitor_ABC.h"
+#include "Subject.h"
 
 namespace depend
 {
+    class FileObserver_ABC;
+
 // =============================================================================
 /** @class  FileVisitor
     @brief  File visitor
 */
 // Created: SLI 2010-08-16
 // =============================================================================
-class FileVisitor : public FileVisitor_ABC
+class FileVisitor : public Subject< FileObserver_ABC >
 {
 public:
     //! @name Types
