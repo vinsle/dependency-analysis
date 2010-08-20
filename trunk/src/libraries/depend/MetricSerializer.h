14a6fb27d99d9314387794e4f069c0af4fb3bbaf
vinsle@gmail.com <vinsle@gmail.com@23fb0dd6-b554-2105-1b3a-678f624095da>
2010-08-20 16:18:15 +0000

    add xml serialization and "high level" metrics computation

---
+++ trunk/src/libraries/depend/MetricSerializer.h
@@ -0,0 +1,94 @@
+//
+//  Copyright Silvin Lubecki 2010
+//
+//  Distributed under the Boost Software License, Version 1.0. (See
+//  accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#ifndef depend_MetricSerializer_h
+#define depend_MetricSerializer_h
+
+#include "DependencyMetricVisitor_ABC.h"
+#include "ClassMetricVisitor_ABC.h"
+#include <set>
+#include <map>
+
+namespace xml
+{
+    class xostream;
+}
+
+namespace depend
+{
+    class DependencyMetric_ABC;
+    class ClassMetric_ABC;
+
+// =============================================================================
+/** @class  MetricSerializer
+    @brief  Metric serializer
+*/
+// Created: SLI 2010-08-20
+// =============================================================================
+class MetricSerializer : private DependencyMetricVisitor_ABC, private ClassMetricVisitor_ABC
+{
+public:
+    //! @name Constructors/Destructor
+    //@{
+             MetricSerializer( const DependencyMetric_ABC& dependencies, const ClassMetric_ABC& classes );
+    virtual ~MetricSerializer();
+    //@}
+
+    //! @name Operations
+    //@{
+    void Serialize( xml::xostream& xos ) const;
+    //@}
+
+private:
+    //! @name Operations
+    //@{
+    virtual void NotifyInternalDependency( const std::string& fromModule, const std::string& toModule, const std::string& include );
+    virtual void NotifyExternalDependency( const std::string& fromModule, const std::string& toModule, const std::string& include );
+    virtual void NotifyClassMetric( const std::string& module, unsigned int classes, unsigned int abstactClasses );
+    //@}
+
+private:
+    //! @name Types
+    //@{
+    typedef std::set< std::string > T_Modules;
+    struct ClassMetrics
+    {
+    public:
+        ClassMetrics()
+            : classes_( 0u )
+            , abstract_( 0u )
+        {}
+        unsigned int classes_;
+        unsigned int abstract_;
+    };
+    typedef std::map< std::string, ClassMetrics > T_ClassMetrics;
+    typedef T_ClassMetrics::const_iterator      CIT_ClassMetrics;
+    typedef std::map< std::string, unsigned int > T_Dependency;
+    typedef std::map< std::string, T_Dependency > T_Dependencies;
+    //@}
+
+private:
+    //! @name Helpers
+    //@{
+    const MetricSerializer::ClassMetrics& FindClass( const std::string& module ) const;
+    //@}
+
+private:
+    //! @name Member data
+    //@{
+    T_Modules modules_;
+    T_ClassMetrics classMetrics_;
+    T_Dependencies afferent_;
+    T_Dependencies efferent_;
+    T_Dependencies external_;
+    //@}
+};
+
+}
+
+#endif // depend_MetricSerializer_h
---
+++ trunk/src/tests/depend_test/MetricSerializerTest.cpp
@@ -0,0 +1,79 @@
+//
+//  Copyright Silvin Lubecki 2010
+//
+//  Distributed under the Boost Software License, Version 1.0. (See
+//  accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#include "depend_test_pch.h"
+#include "depend/MetricSerializer.h"
+#include "MockClassMetric.h"
+#include "MockDependencyMetric.h"
+#include <xeumeuleu/xml.hpp>
+
+using namespace depend;
+
+BOOST_AUTO_TEST_CASE( serialize_metrics_in_xml )
+{
+    MockClassMetric classMetric;
+    MockDependencyMetric dependencyMetric;
+    ClassMetricVisitor_ABC* classVisitor = 0;
+    DependencyMetricVisitor_ABC* dependencyVisitor = 0;
+    MOCK_EXPECT( classMetric, Apply ).once().with( mock::retrieve( classVisitor ) );
+    MOCK_EXPECT( dependencyMetric, Apply ).once().with( mock::retrieve( dependencyVisitor ) );
+    MetricSerializer serializer( dependencyMetric, classMetric );
+    BOOST_REQUIRE( classVisitor );
+    BOOST_REQUIRE( dependencyVisitor );
+    classVisitor->NotifyClassMetric( "module1", 4u, 2u );
+    classVisitor->NotifyClassMetric( "module2", 4u, 2u );
+    dependencyVisitor->NotifyInternalDependency( "module1", "module2", "module2/include" );
+    dependencyVisitor->NotifyInternalDependency( "module2", "module1", "module1/include" );
+    dependencyVisitor->NotifyExternalDependency( "module1", "boost", "boost/assign.hpp" );
+    xml::xostringstream xos;
+    serializer.Serialize( xos );
+    const std::string expected =
+        "<report>"
+        "    <dependencies>"
+        "        <dependency name='module1'/>"
+        "        <dependency name='module2'/>"
+        "    </dependencies>"
+        "    <categories>"
+        "        <category name='module1'>"
+        "            <efferent-dependencies Ce='1'>"
+        "                <dependency name='module2' number='1'/>"
+        "            </efferent-dependencies>"
+        "            <afferent-dependencies Ca='1'>"
+        "                <dependency name='module2' number='1'/>"
+        "            </afferent-dependencies>"
+        "            <external-dependencies Ce='1'>"
+        "                <dependency name='boost' number='1'/>"
+        "            </external-dependencies>"
+        "            <metrics>"
+        "                <number-of-classes>4</number-of-classes>"
+        "                <number-of-abstract-classes>2</number-of-abstract-classes>"
+        "                <abstractness>50</abstractness>"
+        "                <instability>50</instability>"
+        "                <distance>0</distance>"
+        "            </metrics>"
+        "        </category>"
+        "        <category name='module2'>"
+        "            <efferent-dependencies Ce='1'>"
+        "                <dependency name='module1' number='1'/>"
+        "            </efferent-dependencies>"
+        "            <afferent-dependencies Ca='1'>"
+        "                <dependency name='module1' number='1'/>"
+        "            </afferent-dependencies>"
+        "            <external-dependencies Ce='0'/>"
+        "            <metrics>"
+        "                <number-of-classes>4</number-of-classes>"
+        "                <number-of-abstract-classes>2</number-of-abstract-classes>"
+        "                <abstractness>50</abstractness>"
+        "                <instability>50</instability>"
+        "                <distance>0</distance>"
+        "            </metrics>"
+        "        </category>"
+        "    </categories>"
+        "</report>";
+    BOOST_CHECK_XML_EQUAL( expected, xos.str() );
+}
