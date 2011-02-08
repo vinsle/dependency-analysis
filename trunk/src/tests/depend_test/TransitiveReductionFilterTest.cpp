62308b351a7d67121434c1200ca2f5b72d9689c6
vinsle@gmail.com <vinsle@gmail.com@23fb0dd6-b554-2105-1b3a-678f624095da>
2011-02-08 12:43:38 +0000

    build fix

--- trunk/src/tests/depend_test/TransitiveReductionFilterTest.cpp
+++ trunk/src/tests/depend_test/TransitiveReductionFilterTest.cpp
@@ -44,7 +44,7 @@ BOOST_FIXTURE_TEST_CASE( unknown_node_is_filtered, Fixture )
 BOOST_FIXTURE_TEST_CASE( reduction_checks_all_root_nodes, Fixture )
 {
     const T_Edges edges = boost::assign::map_list_of( "a", "b" )( "c", "b" );
-    MOCK_EXPECT( metric, Apply ).once().with( mock::constraint( boost::bind( &Notify, _1, boost::cref( edges ) ) ) );
+    MOCK_EXPECT( metric, Apply ).once().with( boost::bind( &Notify, _1, boost::cref( edges ) ) );
     MOCK_EXPECT( mockFilter, Check ).once().with( "a" ).returns( false );
     MOCK_EXPECT( mockFilter, Check ).once().with( "b" ).returns( false );
     MOCK_EXPECT( mockFilter, Check ).once().with( "c" ).returns( false );
@@ -54,7 +54,7 @@ BOOST_FIXTURE_TEST_CASE( reduction_checks_all_root_nodes, Fixture )
 BOOST_FIXTURE_TEST_CASE( reduction_on_node_filters_any_non_reachable_node, Fixture )
 {
     const T_Edges edges = boost::assign::map_list_of( "a", "b" )( "c", "b" );
-    MOCK_EXPECT( metric, Apply ).once().with( mock::constraint( boost::bind( &Notify, _1, boost::cref( edges ) ) ) );
+    MOCK_EXPECT( metric, Apply ).once().with( boost::bind( &Notify, _1, boost::cref( edges ) ) );
     MOCK_EXPECT( mockFilter, Check ).once().with( "a" ).returns( true );
     MOCK_EXPECT( mockFilter, Check ).once().with( "b" ).returns( false );
     MOCK_EXPECT( mockFilter, Check ).once().with( "c" ).returns( false );
@@ -71,7 +71,7 @@ BOOST_FIXTURE_TEST_CASE( reduction_on_node_filters_any_non_reachable_node, Fixtu
 BOOST_FIXTURE_TEST_CASE( reduction_finds_every_reachable_nodes, Fixture )
 {
     const T_Edges edges = boost::assign::map_list_of( "a", "b" )( "b", "c" );
-    MOCK_EXPECT( metric, Apply ).once().with( mock::constraint( boost::bind( &Notify, _1, boost::cref( edges ) ) ) );
+    MOCK_EXPECT( metric, Apply ).once().with( boost::bind( &Notify, _1, boost::cref( edges ) ) );
     MOCK_EXPECT( mockFilter, Check ).once().with( "a" ).returns( true );
     MOCK_EXPECT( mockFilter, Check ).once().with( "b" ).returns( false );
     MOCK_EXPECT( mockFilter, Check ).once().with( "c" ).returns( false );
