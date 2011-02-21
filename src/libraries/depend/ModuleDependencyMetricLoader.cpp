696b8ad9e0c4a308ee87b41ce9d9a61e98fe009f
vinsle <vinsle@gmail.com>
2011-02-21 13:00:16 +0000

    add a dependency loader

---
+++ src/libraries/depend/ModuleDependencyMetricLoader.cpp
@@ -0,0 +1,91 @@
+//
+//  Copyright Silvin Lubecki 2010
+//
+//  Distributed under the Boost Software License, Version 1.0. (See
+//  accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#include "depend_pch.h"
+#include "ModuleDependencyMetricLoader.h"
+#include "DependencyMetricVisitor_ABC.h"
+#include <xeumeuleu/xml.hpp>
+#include <boost/foreach.hpp>
+
+using namespace depend;
+
+// -----------------------------------------------------------------------------
+// Name: ModuleDependencyMetricLoader constructor
+// Created: SLI 2011-02-18
+// -----------------------------------------------------------------------------
+ModuleDependencyMetricLoader::ModuleDependencyMetricLoader( xml::xisubstream xis )
+{
+    xis >> xml::start( "graph" )
+            >> xml::list( "node", *this, &ModuleDependencyMetricLoader::ReadNode );
+}
+
+// -----------------------------------------------------------------------------
+// Name: ModuleDependencyMetricLoader destructor
+// Created: SLI 2011-02-18
+// -----------------------------------------------------------------------------
+ModuleDependencyMetricLoader::~ModuleDependencyMetricLoader()
+{
+    // NOTHING
+}
+
+// -----------------------------------------------------------------------------
+// Name: ModuleDependencyMetricLoader::Apply
+// Created: SLI 2011-02-18
+// -----------------------------------------------------------------------------
+void ModuleDependencyMetricLoader::Apply( DependencyMetricVisitor_ABC& visitor ) const
+{
+    BOOST_FOREACH( const T_Node& node, nodes_ )
+    {
+        BOOST_FOREACH( const T_Dependency& dependency, node.internals_ )
+            BOOST_FOREACH( const std::string& context, dependency.second )
+                visitor.NotifyInternalDependency( node.from_, dependency.first, context );
+        BOOST_FOREACH( const T_Dependency& dependency, node.externals_ )
+            BOOST_FOREACH( const std::string& context, dependency.second )
+                visitor.NotifyExternalDependency( node.from_, dependency.first, context );
+    }
+}
+
+// -----------------------------------------------------------------------------
+// Name: ModuleDependencyMetricLoader::ReadNode
+// Created: SLI 2011-02-18
+// -----------------------------------------------------------------------------
+void ModuleDependencyMetricLoader::ReadNode( xml::xistream& xis )
+{
+    T_Node node;
+    xis >> xml::attribute( "name", node.from_ )
+        >> xml::start( "efferent-dependencies" )
+            >> xml::list( "dependency", *this, &ModuleDependencyMetricLoader::ReadDependency, node.internals_ )
+        >> xml::end
+        >> xml::start( "external-dependencies" )
+            >> xml::list( "dependency", *this, &ModuleDependencyMetricLoader::ReadDependency, node.externals_ );
+    nodes_.push_back( node );
+}
+
+// -----------------------------------------------------------------------------
+// Name: ModuleDependencyMetricLoader::ReadDependency
+// Created: SLI 2011-02-21
+// -----------------------------------------------------------------------------
+void ModuleDependencyMetricLoader::ReadDependency( xml::xistream& xis, T_Dependencies& dependencies ) const
+{
+    std::string name;
+    T_Contexts contexts;
+    xis >> xml::attribute( "name", name )
+        >> xml::list( "context", *this, &ModuleDependencyMetricLoader::ReadContext, contexts );
+    dependencies.push_back( std::make_pair( name, contexts ) );
+}
+
+// -----------------------------------------------------------------------------
+// Name: ModuleDependencyMetricLoader::ReadContext
+// Created: SLI 2011-02-21
+// -----------------------------------------------------------------------------
+void ModuleDependencyMetricLoader::ReadContext( xml::xistream& xis, T_Contexts& contexts ) const
+{
+    std::string context;
+    xis >> context;
+    contexts.push_back( context );
+}
---
+++ src/tests/depend_test/ModuleDependencyMetricLoaderTest.cpp
@@ -0,0 +1,50 @@
+//
+//  Copyright Silvin Lubecki 2010
+//
+//  Distributed under the Boost Software License, Version 1.0. (See
+//  accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#include "depend_test_pch.h"
+#include "depend/ModuleDependencyMetricLoader.h"
+#include "MockDependencyMetricVisitor.h"
+#include <xeumeuleu/xml.hpp>
+
+using namespace depend;
+
+BOOST_AUTO_TEST_CASE( loader_parse_dependency_xml_and_notifies_visitor )
+{
+    xml::xistringstream xis(
+        "<graph>"
+        "    <node name='from'>"
+        "        <efferent-dependencies>"
+        "            <dependency name='to'>"
+        "                <context>context1</context>"
+        "                <context>context2</context>"
+        "            </dependency>"
+        "        </efferent-dependencies>"
+        "        <external-dependencies>"
+        "            <dependency name='external'>"
+        "                <context>context3</context>"
+        "            </dependency>"
+        "        </external-dependencies>"
+        "    </node>"
+        "    <node name='to'>"
+        "        <efferent-dependencies>"
+        "            <dependency name='from'>"
+        "                <context>context4</context>"
+        "            </dependency>"
+        "        </efferent-dependencies>"
+        "        <external-dependencies/>"
+        "    </node>"
+        "</graph>" );
+    ModuleDependencyMetricLoader loader( xis );
+    MockDependencyMetricVisitor visitor;
+    mock::sequence s;
+    MOCK_EXPECT( visitor, NotifyInternalDependency ).once().in( s ).with( "from", "to", "context1" );
+    MOCK_EXPECT( visitor, NotifyInternalDependency ).once().in( s ).with( "from", "to", "context2" );
+    MOCK_EXPECT( visitor, NotifyExternalDependency ).once().in( s ).with( "from", "external", "context3" );
+    MOCK_EXPECT( visitor, NotifyInternalDependency ).once().in( s ).with( "to", "from", "context4" );
+    loader.Apply( visitor );
+}
