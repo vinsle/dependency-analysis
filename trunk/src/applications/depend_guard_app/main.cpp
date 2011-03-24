dd1f71ce75fdec51fa17fceff86462280562e8f6
vinsle@gmail.com <vinsle@gmail.com@23fb0dd6-b554-2105-1b3a-678f624095da>
2011-03-24 13:21:29 +0000

    dependency guard now parses xml graph generated by depend_app instead of parsing sources

--- trunk/src/applications/depend_guard_app/main.cpp
+++ trunk/src/applications/depend_guard_app/main.cpp
@@ -45,23 +45,21 @@ namespace
                       << "accompanying file LICENSE_1_0.txt or copy at" << std::endl
                       << "http://www.boost.org/LICENSE_1_0.txt)" << std::endl
                       << "See http://code.google.com/p/dependency-analysis for more informations" << std::endl;
-        else if( ! vm.count( "path" ) )
-            throw std::invalid_argument( "Invalid application option argument: missing directories for analysis" );
+        else if( ! vm.count( "graph" ) )
+            throw std::invalid_argument( "Invalid application option argument: missing input graph for analysis" );
         else if( ! vm.count( "dependencies" ) )
-            throw std::invalid_argument( "Invalid application option argument: missing input for dependency check" );
+            throw std::invalid_argument( "Invalid application option argument: missing input for dependency guard check" );
     }
     const bpo::variables_map ParseCommandLine( int argc, char* argv[] )
     {
         bpo::options_description cmdline( "Allowed options" );
         cmdline.add_options()
-            ( "help,h"                                           , "produce help message" )
-            ( "version,v"                                        , "produce version message" )
-            ( "path" , bpo::value< std::vector< std::string > >(), "add a directory containing modules for analysis" )
-            ( "dependencies", bpo::value< std::string >()        , "set input xml file for dependency guard check" );
-        bpo::positional_options_description p;
-        p.add( "path", -1 );
+            ( "help,h"                                   , "produce help message" )
+            ( "version,v"                                , "produce version message" )
+            ( "graph", bpo::value< std::string >()       , "set graph input file for analysis" )
+            ( "dependencies", bpo::value< std::string >(), "set input xml file for dependency guard check" );
         bpo::variables_map vm;
-        bpo::store( bpo::command_line_parser( argc, argv ).options( cmdline ).positional( p ).run(), vm );
+        bpo::store( bpo::command_line_parser( argc, argv ).options( cmdline ).run(), vm );
         bpo::notify( vm );
         CheckOptions( vm, cmdline );
         return vm;
@@ -75,37 +73,12 @@ int main( int argc, char* argv[] )
         const bpo::variables_map vm = ParseCommandLine( argc, argv );
         if( vm.count( "help" ) || vm.count( "version" ) )
             return EXIT_SUCCESS;
-        const std::vector< std::string > paths = vm[ "path" ].as< std::vector< std::string > >();
+        const std::string graph = vm[ "graph" ].as< std::string >();
         const std::string dependencies = vm[ "dependencies" ].as< std::string >();
-        xml::xifstream xis( dependencies );
-        xml::xobufferstream xobs;
-        xobs << xml::start( "configuration" )
-                << xml::start( "paths" );
-        BOOST_FOREACH( const std::string& path, paths )
-            xobs << xml::content( "path", path );
-        xobs    << xml::end
-                << xml::start( "external" )
-                    << xml::content( "includes", "" )
-                    << xml::content( "excludes", "" )
-                << xml::end
-                << xml::start( "extensions" )
-                     << xml::content( "extension", ".h" )
-                     << xml::content( "extension", ".hh" )
-                     << xml::content( "extension", ".hpp" )
-                     << xml::content( "extension", ".hxx" )
-                     << xml::content( "extension", ".inl" )
-                     << xml::content( "extension", ".ipp" )
-                     << xml::content( "extension", ".cxx" )
-                     << xml::content( "extension", ".c" )
-                     << xml::content( "extension", ".cc" )
-                     << xml::content( "extension", ".cpp" )
-                << xml::end
-            << xml::end;
-        xobs >> xml::start( "configuration" );
-        const Facade facade( xobs );
-        BOOST_FOREACH( const std::string& path, paths )
-            facade.Visit( path );
-        if( facade.Process( xis ) )
+        xml::xifstream xisGraph( graph );
+        xml::xifstream xisDependencies( dependencies );
+        const Facade facade( xisGraph );
+        if( facade.Process( xisDependencies ) )
             return EXIT_FAILURE;
         return EXIT_SUCCESS;
     }
