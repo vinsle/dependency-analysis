4504ce6c3dc5690d738e8d4d40e0d07a3fa0d2d9
vinsle <vinsle@gmail.com>
2011-04-05 18:01:35 +0000

    add an application for Strongly Connected Components detection

---
+++ src/applications/depend_components_app/Facade.cpp
@@ -0,0 +1,74 @@
+//
+//  Copyright Silvin Lubecki 2010
+//
+//  Distributed under the Boost Software License, Version 1.0. (See
+//  accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#include "Facade.h"
+#include "depend/Filter_ABC.h"
+#include "depend/ModuleDependencyMetricLoader.h"
+#include "depend/StronglyConnectedComponents.h"
+#include <boost/bind.hpp>
+#include <xeumeuleu/xml.hpp>
+#include <set>
+
+using namespace depend;
+
+// -----------------------------------------------------------------------------
+// Name: Facade constructor
+// Created: SLI 2010-08-18
+// -----------------------------------------------------------------------------
+Facade::Facade( xml::xisubstream xis )
+    : dependencyMetric_( new ModuleDependencyMetricLoader( xis ) )
+{
+    // NOTHING
+}
+
+// -----------------------------------------------------------------------------
+// Name: Facade destructor
+// Created: SLI 2010-08-18
+// -----------------------------------------------------------------------------
+Facade::~Facade()
+{
+    // NOTHING
+}
+
+namespace
+{
+    void Noop() {}
+namespace
+{
+    class SimpleFilter : public Filter_ABC
+    {
+    public:
+        SimpleFilter()
+        {}
+        virtual bool Check( const std::string& /*module*/ ) const
+        {
+            return true;
+        }
+        virtual bool CheckCore( const std::string& /*module*/ ) const
+        {
+            return true;
+        }
+    };
+}
+}
+
+// -----------------------------------------------------------------------------
+// Name: Facade::Process
+// Created: SLI 2010-09-10
+// -----------------------------------------------------------------------------
+void Facade::Process( const std::string& output )
+{
+    boost::shared_ptr< std::ostream > out( &std::cout, boost::bind( &Noop ) );
+    if( !output.empty() )
+        out.reset( new std::ofstream( output.c_str() ) );
+    xml::xostringstream xos;
+    const bool result = StronglyConnectedComponents( *dependencyMetric_ ).Serialize( xos, SimpleFilter() );
+    *out << xos.str();
+    if( !result )
+        throw std::runtime_error( "strongly connected component detected" );
+}
---
+++ src/applications/depend_components_app/Facade.h
@@ -0,0 +1,55 @@
+//
+//  Copyright Silvin Lubecki 2010
+//
+//  Distributed under the Boost Software License, Version 1.0. (See
+//  accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#ifndef depend_metrics_app_Facade_h
+#define depend_metrics_app_Facade_h
+
+#include <boost/noncopyable.hpp>
+#include <memory>
+#include <string>
+
+namespace xml
+{
+    class xisubstream;
+}
+
+namespace depend
+{
+    class DependencyMetricVisitor_ABC;
+    template< typename T > class Visitable;
+}
+
+// =============================================================================
+/** @class  Facade
+    @brief  Facade
+*/
+// Created: SLI 2010-08-18
+// =============================================================================
+class Facade : private boost::noncopyable
+{
+public:
+    //! @name Constructors/Destructor
+    //@{
+    explicit Facade( xml::xisubstream xis );
+    virtual ~Facade();
+    //@}
+
+public:
+    //! @name Operations
+    //@{
+    void Process( const std::string& output );
+    //@}
+
+private:
+    //! @name Member data
+    //@{
+    std::auto_ptr< depend::Visitable< depend::DependencyMetricVisitor_ABC > > dependencyMetric_;
+    //@}
+};
+
+#endif // depend_metrics_app_Facade_h
